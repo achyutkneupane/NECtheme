340596fad49be09452301caeacc530bb814c6e1e
achyutkneupane <aneupane@kumberland.com>
2021-08-22 01:50:27 +0545

    Simple Updates

--- inc/page-single.php
+++ inc/page-single.php
@@ -4,6 +4,15 @@
         <div class="row justify-content-center">
             <div class="col-md-7 box-shadow-full my-3 mx-2">
                 <div class="row">
+                    <div class="col-md-12 w-100 h-auto">
+                        <?php
+                        if (has_post_thumbnail()) {
+                            echo '<img src="'.the_post_thumbnail().'">';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col-md-12 text-muted blackLink">
                         <?php
                                 echo ("<div class='text-muted mb-3 text-left'>");
@@ -18,27 +27,13 @@
                     <div class="col-md-12">
                         <?php
                                 the_content();
-                                dynamic_sidebar('new-sidebar-1');
-                                $post_tags = get_the_tags();
-
-                                if ($post_tags) {
-                                    echo ("<div class='pageText'><span class='text-dark h5'>Tags:</span> ");
-                                    foreach ($post_tags as $tag) {
-                                        if (count($post_tags) > 1) {
-                                            echo '<a href="' . get_tag_link($tag->term_id) . '">' . ucwords($tag->name) . '</a><span class="text-dark">,</span> ';
-                                        } else {
-                                            echo '<a href="' . get_tag_link($tag->term_id) . '">' . ucwords($tag->name) . '</a>';
-                                        }
-                                    }
-                                    echo ("</div>");
-                                }
                                 comments_template();
                                 ?>
                     </div>
                 </div>
             </div>
             <div class="col-md-4 box-shadow-full my-3 mx-2">
-                <?php dynamic_sidebar('sidebar'); ?>
+                <?php get_sidebar('sidebar'); ?>
             </div>
         </div>
     </div>
